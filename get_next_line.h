19d0f6a4ee9c9bd93496506f181ae1bab35db1c6
joshmocniak <42951515+joshmocniak@users.noreply.github.com>
2018-09-05 19:54:29 -0700

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
+# include "42-libft/libft.h"
 # define BUFF_SIZE 32
 
 int					get_next_line(const int fd, char **line);
